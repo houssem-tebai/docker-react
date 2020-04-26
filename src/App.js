278e04cd894d740501542871da0fa7cbd3038fff
root <root@ip-172-31-82-10.ec2.internal>
2020-04-26 12:58:02 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          nharek zin
+          hello from AWS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://merhien-webservice.azurewebsites.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here
         </a>
       </header>
     </div>
