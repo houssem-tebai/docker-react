58b735db788176fb09b006e5804c49b2e0a1581c
Houssem Tebai <houslim12@gmail.com>
2024-02-13 20:06:27 +0100

    added Jenkinsfile

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello from Master branch
+          hello from container
         </p>
         <a
           className="App-link"
